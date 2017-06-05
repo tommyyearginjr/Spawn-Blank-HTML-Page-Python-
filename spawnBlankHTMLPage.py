8d48b1609d5796b1ded6ad95c381ef6d1eb7a8b9
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-06-05 13:47:27 -0400

    commit

--- spawnBlankHTMLPage.py
+++ spawnBlankHTMLPage.py
@@ -6,11 +6,11 @@ mimei = list(i.strip() for i in michael)
 f.close()
 def Create_Page():
     f = open(page_name,'a')
-    for i in range(0,mimei.index("<script>")):
+    for i in range(0,mimei.index("</body>")):
         f.write(mimei[i]+'\n')
     for i in range(1,int(how_many_p_elements)+1):
         f.write('<p id="{}"></p>\n'.format(i))
-    for i in range(mimei.index("<script>"),len(mimei)):
+    for i in range(mimei.index("</body>"),len(mimei)):
         f.write(mimei[i]+'\n')
     f.close()
 Create_Page()
